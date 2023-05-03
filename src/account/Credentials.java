661ad2ed60f83ac281a953348cdaf9902a7a2b45
nourhan031 <92525531+nourhan031@users.noreply.github.com>
2023-05-03 10:36:08 +0300

    Update Credentials.java
    initial register operation

--- src/account/Credentials.java
+++ src/account/Credentials.java
@@ -1,4 +1,42 @@
 package account;
 
 public class Credentials {
+   private String[] email;
+    private String[] password;
+    private int numUsers;
+
+    public Credentials(int capacity) {
+        email = new String[capacity];
+        password = new String[capacity];
+        numUsers = 0;
+    }
+
+    public boolean addUser(String email, String password) {
+        // Check if email already exists in the array
+        for (int i = 0; i < numUsers; i++) {
+            if (email[i].equals(email)) {
+                return false;
+            }
+        }
+
+        // Add user to the array
+        email[numUsers] = email;
+        password[numUsers] = password;
+        numUsers++;
+
+        return true;
+    }
+
+    public boolean checkUser(String e, String p) {
+        // Check if email and password match a user in the array
+        for (int i = 0; i < numUsers; i++) {
+            if (email[i].equals(e) && password[i].equals(p)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+}
 }
