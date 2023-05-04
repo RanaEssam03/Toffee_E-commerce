0154bdba985e59cc9ab3a3c524e62196a302b6bc
nourhan031 <nourhanabdallah031@gmail.com>
2023-05-05 00:32:05 +0300

    Added Address class

--- src/account/Address.java
+++ src/account/Address.java
@@ -1,4 +1,30 @@
 package account;
 
 public class Address {
+        private String street;
+        private String city;
+        public Address(String street, String city) {
+            this.street = street;
+            this.city = city;
+        }
+
+        // Setters
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+       // Getters
+       public String getStreet() {
+        return street;
+       }
+
+       public String getCity() {
+       return city;
+       }
+
+
 }
